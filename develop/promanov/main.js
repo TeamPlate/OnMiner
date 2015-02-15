b9501c970b19265a69dbeae8c27d7faaa411c793
PomanoB <romanov4400@gmail.com>
2015-02-15 19:46:43 +0300

    Чат, #8

--- develop/promanov/main.js
+++ develop/promanov/main.js
@@ -13,6 +13,30 @@ forceResize.emit();
 Kefir.fromEvent($('#main-top-menu-open-button'), 'click').onValue(function(el){
 	$('.main-top-menu').toggleClass('opened');
 });
+Kefir.fromEvent($('#main-chat-menu-open-button'), 'click').onValue(function(el){
+	$('.main-chat').toggleClass('opened');
+});
+
+var chat = [];
+for(var i = 10; i < 50; i++)
+{
+	chat.push({
+		time: '18:40:' + i,
+		msg: (Math.random() * 1000000000000).toString(2),
+		user: (Math.random() * 5 + 1) | 0
+	});
+}
+
+$('#main-chat-output-area').val(chat.map(function(chatMsg){
+	return [
+		'[',
+		chatMsg.time,
+		']',
+		'Bot#' + chatMsg.user,
+		':',
+		chatMsg.msg
+	].join(' ');
+}).join('\n'));
 
 //var GameScoreTimer = require('./web/view/game-score-timer.jsx');
 //React.render(
